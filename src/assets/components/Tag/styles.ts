b31c6158ab28c3ed4c0ab83603ba6e0d58d1ab0a
Augusto Cedric <cedric.pmi.rmp@gmail.com>
2024-03-27 11:27:44 -0300

    configuração do titulo e preço, button do banner

---
+++ src/assets/components/Tag/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+import { cores } from '../../../styles'
+
+import { Props } from '.'
+
+export const TagContainer = styled.div<Props>`
+  background-color: ${cores.verde};
+  color: ${cores.czClaro};
+  font-size: ${(props) =>
+    props.size === 'big'
+      ? '16px'
+      : '10px'}; //Passando uma função props, para quando for 'small' e 'big'
+  font-weight: bold;
+  padding: ${(props) =>
+    props.size === 'big'
+      ? '8px 16px'
+      : '4px 6px'}; //Passando uma função props, para quando for 'small' e 'big'
+  border-radius: 8px;
+  display: inline-block;
+`
