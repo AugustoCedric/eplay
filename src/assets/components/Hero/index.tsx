1ce27b7a500aa9cd9bd54572765d05690f9a52e1
Augusto Cedric <cedric.pmi.rmp@gmail.com>
2024-03-30 09:51:44 -0300

    criação de rotas pelo id e add Hero

---
+++ src/assets/components/Hero/index.tsx
@@ -0,0 +1,8 @@
+import bannerImage from '../../images/fundo_hogwarts.png'
+import { Banner } from './styles'
+
+const Hero = () => (
+  <Banner style={{ background: `url(${bannerImage})` }}>hero</Banner>
+)
+
+export default Hero
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 
 import Categories from './Pages/Categories'
 import Home from './Pages/Home'
+import Product from './Pages/Product'
 
 // Criando as Rotas
 const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/categories" element={<Categories />} />
+    <Route path="/product/:id" element={<Product />} />
   </Routes>
 )
 
