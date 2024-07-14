0ac82b0218a1503dae43878f43c007147f78013f
Augusto Cedric <cedric.pmi.rmp@gmail.com>
2024-07-14 13:13:32 -0300

    configuração do Reduxtoolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import Header from './assets/components/Header'
@@ -6,17 +7,20 @@ import { GlobalCss } from './styles'
 // importando as rotas de navegação do site
 import Footer from './assets/components/Footer'
 import Rotas from './routes'
+import { store } from './store'
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalCss />
-      <div className="container">
-        <Header />
-      </div>
-      <Rotas />
-      <Footer />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalCss />
+        <div className="container">
+          <Header />
+        </div>
+        <Rotas />
+        <Footer />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
